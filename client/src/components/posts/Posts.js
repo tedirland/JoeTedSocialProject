f40c43b44a5a0cc3b0a443aed12473f04adae9ed
Ted Irland <irlandth@gmail.com>
2021-01-31 09:53:53 -0600

    Hotfix for user name PUNC

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -14,7 +14,7 @@ const Posts = ({ getPosts, post: { posts }, auth:{user} }) => {
     <Fragment>
       <h1 className="large text-primary">Posts</h1>
       <p className="lead">
-        <i className="fas fa-user" />Welcome {user && user.name} Dive Into The Discussion
+        <i className="fas fa-user" />Welcome, {user && user.name}! Dive Into The Discussion
       </p>
       <PostForm />
       <div className="posts">
